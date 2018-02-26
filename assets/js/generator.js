0fc501f751685bb3274e0880e150137f2c70b3c7
nyinyisweetiepie <ntmbayfield@gmail.com>
2018-02-25 19:20:57 -0800

    Update generator.js

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -1,9 +1,9 @@
 
 
-const colors = {"A": "Adamantium", "B": "Blue", "C": "Coral"}
-const nouns = ["Warrior", "Sword", "Tiger"] // refactor to object literal like colors
-const adjectives = ["Destroyer", "Enslaver", "Eviscerator"]
-const origin = ["Gotham", "Death", "Nature"] // refactor to object literal like colors
+const firstLetterOfFName = {"A": "Red", "B": "Dark", "C": "Grey", "D": "Tidal", "E": "Tri", "F": "Strike", "G": "Poison", "H": "Flare", "I": "Blue", "J": "Killer", "K": "Black", "L": "Infinity", "M": "Star", "N": "Pale", "O": "Crazed", "P": "Quake", "R": "Sarsic", "S": "White", "T": "Devil", "U": "Green", "V": "Silver", "W": "Psion", "X": "Arctic", "Y": "Green", "Z": "Storm"}
+const firstLetterofLName = {"A": "Warrior", "B": "Doctor", "C": "Tempter", "D": "Shot", "E": "Doom", "F": "Sword", "G": "Stalker", "H": "Killer", "I": "Slicer", "J": "Thorn", "K": "Fist", "L": "Tyrant", "M": "Lord", "N": "Professor", "O": "Dragon", "P": "Whip", "Q": "Nymph", "R": "Tiger", "S": "Cyborg", "T": "Wizard", "U": "Light", "V": "Brain", "W": "One", "X": "Beast", "Y": "Croc", "Z": "Born"} // refactor to object literal like colors
+const dayOfBirth = ["The Trickster", "The Destroyer", "The Terror", "The Ruler", "The Murderer", "The Richest", "The Monarch", "The Master", "The Enslaver", "The Horror", "The Mistake", "The Mercenary", "The Keeper", "The Rogue", "The General", "The Reviled", "The Smiter", "The Eviscerator", "The Queller", "The Abolisher", "The Silencer", "The Watcher", "The Assassin", "The Pestilence", "The Hated", "The Feared", "The Dreaded", "The Survivor", "The Immortal", "The Consort", "The Demon"]
+const firstLetterOfMName = {"A": "of Men", "B": "of Hope", "C": "of Europe", "D": "of Gotham", "E": "of Women", "F": "of The Ancient Ones", "G": "of Death", "H": "of Time", "I": "of Humanity", "J": "of Earth", "K": "of The States", "L": "of The Future", "M": "of Asia", "N": "of Mars", "O": "of The Galaxy", "P": "of The Stars", "Q": "of Nature", "R": "of the Living", "S": "of Metropolis", "T": "of Law", "U": "of Freedom", "V": "of Knowledge", "W": "of Lust", "X": "of Space", "Y": "of Faith", "Z": "of Asia"} // refactor to object literal like colors
 
 let generator = function(userInfo) {
   // get the first letter of the user's first name
